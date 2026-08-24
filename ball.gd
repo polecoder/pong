3f420c9ff1d9ec0ac5ae27d4ee391828488c1df2
polecoder <maurop4502@gmail.com>
2026-08-23 22:53:56 -0300

    pelota con movimiento configurado, faltan colisiones con pared

---
+++ ball.gd
@@ -0,0 +1,17 @@
+extends CharacterBody2D
+
+var screen_size
+var direction
+@export var speed = 400
+
+func _ready() -> void:
+	screen_size = get_viewport_rect().size
+	# velocidad inicial randomizada
+	direction = Vector2.RIGHT.rotated(randf_range(0, TAU))
+
+
+func _physics_process(delta: float) -> void:
+	velocity = direction * speed
+	var collision = move_and_collide(velocity * delta)
+	if collision:
+		velocity = velocity.bounce(collision.get_normal())
